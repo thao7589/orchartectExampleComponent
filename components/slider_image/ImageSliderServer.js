369306ab933a520defb0d8d4d5a304c94776c501
thao <leanhthao01@gmail.com>
2020-02-04 00:56:53 +0700

    update files

---
+++ components/slider_image/ImageSliderServer.js
@@ -0,0 +1,32 @@
+var PageComponent = require("ds.base/PageComponent");
+
+var ImageSliderServer = PageComponent.create({
+	data: function(attributes, vars, containerList) {
+		var urlArray = [];
+
+		//Search for all upload records
+		var fr = new FRecord('upload');
+		fr.search();
+		while (fr.next()) {
+			//Check to see if we need to match a suffix
+			if (attributes.suffix != 'all') {
+				//Check to see if the suffix matches
+				if (fr.name.indexOf(attributes.suffix) >= 0) {
+					//Add the url to the list
+					urlArray.push(fr.url);
+ 				}
+			} else {
+				//Pushing all urls
+				urlArray.push(fr.url);
+			}		
+		}
+		
+		return new StatusResponse('good', {
+			urls: urlArray
+		});
+	},
+	
+	type: "ImageSliderServer"
+});
+
+module.exports = ImageSliderServer;
